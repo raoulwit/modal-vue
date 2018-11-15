c3ee778a22cab3118017d0a441ef47cc78954a2c
Raoul Witsenburg <raoulwitsenburg@gmail.com>
2018-11-15 14:36:39 +0100

    modal bijna helemaal

---
+++ main.js
@@ -0,0 +1,38 @@
+Vue.component('modal', {
+
+data() {
+  return {
+    isVisible: true
+  };
+},
+
+template:`
+<div class="modal is-active">
+
+  <div class="modal-background"></div>
+
+  <div class="modal-content">
+
+    <div class="box">
+
+      <p>Lorem IPPPPPSSSSUUUM</p>
+
+    </div>
+
+
+  </div>
+
+  <button class="modal-close is-large" aria-label="close"></button>
+
+</div>
+`
+
+});
+
+new Vue({
+  el: "#root",
+
+  data: {
+    showModal: false
+  }
+});
